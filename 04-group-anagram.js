9e325c6b3b97627538fcc32547e73107a38d90cc
danielolasupo02 <danielolasupo02@gmail.com>
2025-02-20 14:52:54 +0100

    feat: add optimal solution for 'Group Anagrams' (Blind 75 #4)

--- 04-group-anagram.js
+++ 04-group-anagram.js
@@ -23,37 +23,65 @@
 
 
 
-function groupAnagramBruteForce(strs) {
-    const length = strs.length;
+// function groupAnagramBruteForce(strs) {
+//     const length = strs.length;
 
-    let result = [];
-    let visited = Array(length).fill(false);
+//     let result = [];
+//     let visited = Array(length).fill(false);
 
-    for (let i=0; i<length; i++) {
-      if (visited[i]) continue //skips visited element
+//     for (let i=0; i<length; i++) {
+//       if (visited[i]) continue //skips visited element
 
-      visited[i] = true;
-      let group = [strs[i]]
+//       visited[i] = true;
+//       let group = [strs[i]]
 
-      for (let j=i+1; j<length; j++) {
-        if(!visited[j]  &&  isAnagram(strs[i], strs[j])) {
-            visited[j] = true
-            group.push(strs[j])
-        }
-      } 
+//       for (let j=i+1; j<length; j++) {
+//         if(!visited[j]  &&  isAnagram(strs[i], strs[j])) {
+//             visited[j] = true
+//             group.push(strs[j])
+//         }
+//       } 
       
-      result.push(group);
-    }
+//       result.push(group);
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
-function isAnagram(s1, s2) {
-    if(s1.length !== s2.length) return false
-    return (s1.split('').sort().join() === s2.split('').sort().join())
-}
+// function isAnagram(s1, s2) {
+//     if(s1.length !== s2.length) return false
+//     return (s1.split('').sort().join() === s2.split('').sort().join())
+// }
 
-console.log(groupAnagramBruteForce(["eat", "tea", "tan", "ate", "nat", "bat"]))
+// console.log(groupAnagramBruteForce(["eat", "tea", "tan", "ate", "nat", "bat"]))
+// console.log(groupAnagramBruteForce([""]))
+// console.log(groupAnagramBruteForce(["a"]))
 
 // Time Complexity => O(n² × k log k)
 // Space Complexity => O(n)
+
+
+
+function groupAnagramOptimal(strs) {
+  const map = new Map();
+
+  for (let str of strs) {
+    let sortedStr = str.split('').sort().join('');
+
+    if(!map.has(sortedStr)) {
+      map.set(sortedStr, [])
+    }
+    
+    map.get(sortedStr).push(str)
+    
+
+    
+  }
+
+  return Array.from(map.values())
+}
+
+console.log(groupAnagramOptimal(["eat", "tea", "tan", "ate", "nat", "bat"]))
+
+// Time-Compexity => O(n × k log k)
+// Space-Complexity => O(n × k)
