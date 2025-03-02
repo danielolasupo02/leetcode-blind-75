5166fa0d7dde4445991f84b00265c26b8b826bfe
danielolasupo02 <danielolasupo02@gmail.com>
2025-03-02 23:40:46 +0100

    feat: add optimal solution for 'Top K Frequent Elements' (Blind 75 #5)

--- 05-top-k-frequent-elements.js
+++ 05-top-k-frequent-elements.js
@@ -17,37 +17,59 @@
 // Input: nums = [1,1,1,2,2,3], k = 2
 // Output: [1,2]
 
-
 // function topKFrequentBruteForce(nums, k) {
+// 	// create an empty hashmap
 // 	const freqMap = new Map();
 
+// 	// get the corresponding array element's frequency map
 // 	for (let num of nums) {
-// 		freqMap.set(num, (freqMap.get(num) || 0) + 1)
+// 		freqMap.set(num, ((freqMap.get(num) || 0) + 1))
 // 	}
 
-// 	const sorted = [...freqMap.entries()].sort((a,b) => b[1] - a[1])
-
-// 	const topK = sorted.slice(0, k).map(entry => entry[0])
-
-// 	return topK;
+// 	// sort the frequency map in descending order - highest to lowest
+// 	const sorted = [...freqMap.entries()].sort( (a,b) => b[1] - a[1])
+	
+// 	// get the top K frequent elements in the sorted array
+// 	const topK = sorted.slice(0, k).map((entry) => entry[0])
+	
+// 	// return the result
+// 	return topK
 // }
 
+// console.log(topKFrequentBruteForce([1,1,1,2,2,3], 2))
 
-function topKFrequentBruteForce(nums, k) {
-	const freqMap = new Map();
+// console.log(topKFrequentBruteForce([1], 2))
 
-	for (let num of nums) {
-		freqMap.set(num, ((freqMap.get(num) || 0) + 1))
-	}
 
-	const sorted = [...freqMap.entries()].sort( (a,b) => b[1] - a[1])
-	
-	const topK = sorted.slice(0, k).map((entry) => entry[0])
-	
-	return topK
+// // Time-Complexity => O(N log N)
+// // Space-Complexity => O(N)
+
+
+function topKFrequentOptimal(nums, k) {
+    // Step 1: Build a frequency Map
+    const freqMap = new Map();
+    for (let num of nums) {
+        freqMap.set(num, (freqMap.get(num) || 0) + 1)
+    }
+
+    // Step 2: Create buckets based on frequency
+    let buckets = Array.from({length: nums.length + 1}, () => ([]))
+    for (const [num,freq] of freqMap.entries()) {
+        buckets[freq].push(num)
+    }
+    
+    // Step 3: Fetch Only Top K Elements
+    const result = [];
+    for (let i = buckets.length -1; i>=0 && result.length < k; i--) {
+        if(buckets[i]) {
+            result.push(...buckets[i])
+        }
+    }
+    return result.slice(0, k)
+    
 }
 
+console.log(topKFrequentOptimal([1,1,1,2,2,3], 2))
 
-console.log(topKFrequentBruteForce([1,1,1,2,2,3], 2))
-
-console.log(topKFrequentBruteForce([1], 2))
+// // Time-Complexity => O(N)
+// // Space-Complexity => O(N + K)
